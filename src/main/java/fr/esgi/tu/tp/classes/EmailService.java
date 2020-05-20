6a836f50f00ae1042035877ebdcc66d1c6155762
Gwen <gwendou95@hotmail.fr>
2020-05-20 22:47:16 +0200

    Add mock on EmailService.send()

--- src/main/java/fr/esgi/tu/tp/classes/EmailService.java
+++ src/main/java/fr/esgi/tu/tp/classes/EmailService.java
@@ -7,6 +7,9 @@ import java.time.LocalDate;
 @Builder
 public class EmailService {
 
+    public boolean send() throws Exception {
+        throw new Exception("This need to be mock");
+    }
 
     public boolean sendEmail(User user){
            if(LocalDate.now().minusYears(18).isAfter(user.getBirthday())){
--- src/test/java/fr/esgi/tu/tp/classes/EmailServiceTest.java
+++ src/test/java/fr/esgi/tu/tp/classes/EmailServiceTest.java
@@ -3,19 +3,46 @@ package fr.esgi.tu.tp.classes;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-
 public class EmailServiceTest {
+
+//    @Mock
     private EmailService sut;
 
     @Before
-    public void mySetUp() {
+    public void setUp() {
+//        MockitoAnnotations.initMocks(this);
         this.sut = EmailService.builder().build();
     }
 
+    @Test
+    public void testSendNominal(){
+        Assert.assertThrows(Exception.class, () ->{
+            this.sut.send();
+        });
+    }
+
+    @Test
+    public void testMockSendTrue() throws Exception {
+        EmailService emailService = Mockito.mock(EmailService.class);
+        Mockito.when(emailService.send()).thenReturn(true);
+//        Mockito.when(this.sut.send()).thenReturn(true);
+        Assert.assertTrue(emailService.send());
+    }
+
+    @Test
+    public void testMockSendFalse() throws Exception {
+        EmailService emailService = Mockito.mock(EmailService.class);
+        Mockito.when(emailService.send()).thenReturn(false);
+        Assert.assertFalse(emailService.send());
+    }
 
     @Test
     public void sendEmailNominal(){
@@ -42,4 +69,7 @@ public class EmailServiceTest {
 
         Assert.assertFalse(this.sut.sendEmail(user));
     }
+
+
+
 }
